d4d5ee09e3248479650e2c24d01d3d3761a03e85
Ricardo Rocha <me@rrocha.uk>
2021-04-12 16:00:52 +0100

    first commit

---
+++ editor.js
@@ -0,0 +1,61 @@
+const menu = document.querySelector('#buttonMenu');
+let textArea = document.getElementById('textArea');
+
+menu.addEventListener('click', event => {
+
+    if(event.target.matches('button')) {
+
+        const button = event.target; //set the clicked button
+        const action = button.dataset.actions;
+
+        switch (action) {
+            case 'bold':
+                setBold()
+                break;
+            case 'italic':
+                setItalic()
+                break;
+            case 'underline':
+                setUnderline()
+                break;
+        }
+
+    }
+
+})
+
+const setBold = () => {
+    selection('bold');
+}
+const setItalic = () => {
+    selection('italic')
+}
+const setUnderline = () => {
+    selection('underline')
+}
+
+
+const selection = (type) => {
+    let length = textArea.value.length;
+    let selectionStart = textArea.selectionStart;
+    let selectionEnd = textArea.selectionEnd;
+
+    let currentSelection = textArea.value.substring(selectionStart, selectionEnd);
+
+    let replace;
+    if(type === 'bold') {
+        replace = '<b>' + currentSelection + '</b>';
+    }
+    if(type === 'italic') {
+        replace = '<i>' + currentSelection + '</i>';
+    }
+    if(type === 'underline') {
+        replace = '<u>' + currentSelection + '</u>';
+    }
+
+    textArea.value = textArea.value.substr(0, selectionStart) + replace + textArea.value.substr(selectionEnd, length);
+
+}
+
+
+//code for preview - to be removed later
